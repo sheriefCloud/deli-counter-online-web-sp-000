3015bccad9af1b3c9c2dc3280687861ffea9fdc8
Sherief Farid <sherief@farid.cloud>
2019-03-02 18:15:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  if katz_deli.size == 0 
+  if katz_deli.size == 0
     message = "The line is currently empty."
   else
     katz_deli
   end
-  
+
 end
