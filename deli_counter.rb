417ec65089ad48f6c1d868eab51b945cbdd17e90
Sherief Farid <sherief@farid.cloud>
2019-03-02 18:22:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 # Write your code here.
 katz_deli = []
 message = "The line is currently: "
-# counter = 0
+ counter = 0
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    # until counter == katz_deli.size
-    #   message = message + #{counter} + #{katz_deli[counter]}
-    #   counter += 1
-
+    until counter == katz_deli.size do
+      message = message + #{counter} + #{katz_deli[counter]}
+      counter += 1      
+    end
+      return message
   end
-  return message
+  
 end
