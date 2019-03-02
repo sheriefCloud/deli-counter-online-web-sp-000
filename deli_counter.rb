ab93b0c691183300e1c95731bfc2edcfc15c47b7
Sherief Farid <sherief@farid.cloud>
2019-03-02 18:48:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def take_a_number(katz_deli,person)
 
     katz_deli << person
-    puts "Welcome, #{person}. You are currently Number #{katz_deli.size} in line."
+    puts "Welcome, #{person}. You are Number #{katz_deli.size} in line."
 
 
 
